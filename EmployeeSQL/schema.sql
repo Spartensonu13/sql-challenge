e54f71c6156f2ca5e28c7de93bd61f280a1f5b4b
Spartensonu13 <164107079+Spartensonu13@users.noreply.github.com>
2024-06-03 13:58:25 -0400

    Module 9 Challenge

---
+++ EmployeeSQL/schema.sql
@@ -0,0 +1,50 @@
+DROP TABLE IF EXISTS TITLES;
+CREATE TABLE TITLE(
+	TITLE_ID VARCHAR,
+	TITLE VARCHAR,
+	PRIMARY KEY(TITLE_ID)
+);
+
+CREATE TABLE EMPLOYEES(
+	emp_no INT  NOT NULL,
+    emp_title_id VARCHAR NOT NULL,
+    birth_date DATE NOT NULL,
+    first_name VARCHAR  NOT NULL,
+    last_name VARCHAR NOT NULL,
+    sex VARCHAR  NOT NULL,
+    hire_date DATE NOT NULL,
+	FOREIGN KEY(emp_title_id) REFERENCES Title(TITLE_ID),
+	PRIMARY KEY (emp_no)
+);
+
+CREATE TABLE departments(
+	dept_no VARCHAR NOT NULL,
+	dept_name Varchar NOT NULL,
+	PRIMARY KEY(dept_no)
+);
+
+CREATE TABLE dept_manager (
+	dept_no VARCHAR NOT NULL,
+	emp_no INT NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+	PRIMARY KEY (emp_no, dept_no)
+	
+);
+
+
+CREATE TABLE dept_emp(
+	emp_no INT NOT NULL,
+	dept_no VARCHAR NOT NULL,
+	FOREIGN KEY (emp_no)  REFERENCES employees (emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+	PRIMARY KEY (emp_no, dept_no)
+	
+);
+
+CREATE TABLE salaries (
+	emp_no INT NOT NULL,
+	salary INT NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES EMPLOYEES (emp_no),
+	PRIMARY KEY (emp_no)
+);
